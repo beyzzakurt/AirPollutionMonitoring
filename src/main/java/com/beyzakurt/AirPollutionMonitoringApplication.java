79accda5beda792d3cc7a0d9dd0b59fc4ce5a13a
beyzzakurt <beyzzakurt34@gmail.com>
2025-04-27 10:26:18 +0300

    Initial commit

---
+++ src/main/java/com/beyzakurt/AirPollutionMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.beyzakurt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirPollutionMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirPollutionMonitoringApplication.class, args);
+    }
+
+}
