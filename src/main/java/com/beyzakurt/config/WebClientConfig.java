ea05c53561edd6560d517309063f1b458b070668
beyzzakurt <beyzzakurt34@gmail.com>
2025-04-30 14:47:00 +0300

    Configure for WebClient

---
+++ src/main/java/com/beyzakurt/config/WebClientConfig.java
@@ -0,0 +1,15 @@
+package com.beyzakurt.config;
+
+import com.beyzakurt.service.ExternalApiProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(ExternalApiProperties externalApiProperties) {
+        return WebClient.builder().baseUrl(("https://api.waqi.info")).build();
+    }
+}
