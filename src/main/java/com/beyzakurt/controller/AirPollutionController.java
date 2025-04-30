6d16fe24dfe04e90215419f15547f82d7425d20d
beyzzakurt <beyzzakurt34@gmail.com>
2025-04-30 15:00:56 +0300

    Add GET, POST mappings to handle requests

---
+++ src/main/java/com/beyzakurt/controller/AirPollutionController.java
@@ -0,0 +1,28 @@
+package com.beyzakurt.controller;
+
+import com.beyzakurt.model.AirPollutionModel;
+import com.beyzakurt.service.AirPollutionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/rest/api/air-pollution")
+@RequiredArgsConstructor
+public class AirPollutionController {
+
+    private final AirPollutionService airPollutionService;
+
+    @GetMapping("/{city}")
+    public Mono<AirPollutionModel> getAirPollution(@PathVariable String city) {
+        return airPollutionService.getAirPollutionData(city);
+    }
+
+
+    @PostMapping("/add")
+    public Mono<String> addAirQualityData(@RequestBody AirPollutionModel data) {
+        return airPollutionService.saveAirPollutionData(data);
+    }
+
+
+}
