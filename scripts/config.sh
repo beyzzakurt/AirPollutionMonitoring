8aa8be057d6db27912bb68a9f88766d9b86b8b4c
beyzzakurt <beyzzakurt34@gmail.com>
2025-05-12 20:53:52 +0300

    Create a config for scripts

---
+++ scripts/config.sh
@@ -0,0 +1,4 @@
+export INFLUX_URL="http://localhost:8086"
+export TOKEN="ye6JWmAsopJdcom-khY0tB5ichosO6nqOWpaeeAz9JelZfX078y3bB5aqtkXWVcsS3rOqvTO2y5JqEUVjOJ5_Q=="
+export ORG="AirPollution"
+export BUCKET="AirPollutionData"
